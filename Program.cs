4a40bb5ae466a1c01b31bc0f0d146d58e79e9a9b
Nermohm <111206466+Nermohm@users.noreply.github.com>
2025-05-07 16:00:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,37 +1,32 @@
-﻿namespace ConsoleApp13
+namespace ConsoleApp2
 {
     internal class Program
     {
-
-        static int Num()
-        {
-            
-                Random n = new Random();
-                int u = n.Next(1000, 9999);
-
-                Console.Write(u);
-                
-            
-            return u;
-        }
-        static void chart()
-        {
-            string lol = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@#$%^&*()_+";
-            for (int i = 1;i < 16; i++)
-            {
-                Random n = new Random();
-                int u = n.Next(0, 65);
-                Console.Write(lol[u]);
-
-            }
-            
-        }
         static void Main(string[] args)
         {
+           
+
             
-            Num();
-            Console.WriteLine();
-            chart();
+            while (true)
+            {
+                Console.WriteLine("Choose your operation\n");
+                string input = Console.ReadLine().Trim();
+                var index = input.IndexOf(' ');
+                var command = input.Substring(0, index);
+                var path = input.Substring(index + 1).Trim();
+                if(command == "ls")
+                {
+                    foreach(var e in Directory.GetDirectories(path))
+                    {
+                        Console.WriteLine($"[Directory] : {e}");
+                    }
+                    foreach(var e in Directory.GetFiles(path))
+                    {
+                        Console.WriteLine($"[File] : {e}");
+                    }
+                }
+                
+            }
         }
     }
 }
