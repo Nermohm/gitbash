36ddac902d8841da495cf886bfcd9468d648311d
Nermohm <111206466+Nermohm@users.noreply.github.com>
2025-05-10 14:14:54 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,29 +4,74 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-           
+            /************************Variables declarations************************/
+            string input;
+            int whitespace_index;
+            string command;
+            string path;
+
+            /****************************Code**************************************/
+
+            Console.WriteLine("\t\t\tWelcome to Tarek's BASH");
 
             
+
+            /************************Bash function*********************************/
+
             while (true)
             {
-                Console.WriteLine("Choose your operation\n");
-                string input = Console.ReadLine().Trim();
-                var index = input.IndexOf(' ');
-                var command = input.Substring(0, index);
-                var path = input.Substring(index + 1).Trim();
-                if(command == "ls")
+                /************************Taking input**********************************/
+
+                input = Console.ReadLine().Trim();
+                whitespace_index = input.IndexOf(' ');
+                command = input.Substring(0, whitespace_index);
+                path = input.Substring(whitespace_index + 1).Trim();
+                if (command == "ls")
                 {
-                    foreach(var e in Directory.GetDirectories(path))
+                    foreach (var entry in Directory.GetDirectories(path))
                     {
-                        Console.WriteLine($"[Directory] : {e}");
+                        Console.WriteLine($"\t[Dir] {entry}");
                     }
-                    foreach(var e in Directory.GetFiles(path))
+                    foreach (var entry in Directory.GetFiles(path))
                     {
-                        Console.WriteLine($"[File] : {e}");
+                        Console.WriteLine($"\t[File] {entry}");
                     }
                 }
-                
+                else if(command == "info")
+                {
+                    if (Directory.Exists(path))
+                    {
+                        var info = new DirectoryInfo(path);
+                        Console.WriteLine("Type : Directory");
+                        Console.WriteLine($"Created at : {info.CreationTime}");
+                        Console.WriteLine($"Last access time at : {info.LastAccessTime}");
+                    }
+                    else if (File.Exists(path))
+                    {
+                        var info = new FileInfo(path);
+                        Console.WriteLine("Type : File");
+                        Console.WriteLine($"Created at : {info.CreationTime}");
+                        Console.WriteLine($"Last access time at : {info.LastAccessTime}");
+                    }
+                }
+                else if(command == "mkdir")
+                {
+                    Directory.CreateDirectory(path);
+                }
+                else if(command == "cat")
+                {
+                    var content = File.ReadAllText(path);
+                    Console.WriteLine(content);
+                }
+                else if(command == "exit")
+                {
+                    break;
+                }
             }
+            
+                
+            
         }
     }
 }
+
